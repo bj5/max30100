c77f015a82baa57b98ad115eb5e8ff3fa577c893
Jerry Liu <j5liu@yahoo.com>
2018-05-24 16:10:12 +0800

    add demo

---
+++ examples/demo.py
@@ -0,0 +1,8 @@
+import max30100
+mx30 = max30100.MAX30100()
+mx30.refresh_temperature()
+temp = mx30.get_temperature()
+print('TEMP=%d'%temp)
+reg = mx30.get_registers()
+mx30.read_sensor()
+mx30.ir, mx30.red
